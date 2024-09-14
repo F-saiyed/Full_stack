c2cd344f560664535a59f0500c9259e9a0f7e78e
Fehmidabanu Saiyed <fsaiy083@uottawa.ca>
2024-09-13 22:25:26 -0400

    Delete extend.py

--- extend.py
+++
@@ -1,9 +0,0 @@
-# Using append
-list1 = [1, 2, 3]
-list1.append([4, 5])
-print("After append:", list1)  # Output: [1, 2, 3, [4, 5]]
-
-# Using extend
-list2 = [1, 2, 3]
-list2.extend([4, 5])
-print("After extend:", list2)  # Output: [1, 2, 3, 4, 5]
